ef0ca7577382d8b8d0c9a5516e9f09962ea74638
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2023-11-21 10:39:04 +0700

    json-ld

--- material-ui-nextjs-ts/src/app/(user)/layout.tsx
+++ material-ui-nextjs-ts/src/app/(user)/layout.tsx
@@ -1,16 +1,29 @@
 import AppBottom from '@/components/bottom/app.bottom'
 import AppHeader from '@/components/header/app.header'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Discover songs in SoundCloud',
   description: 'Discover the top streamed music and songs online on SoundCloud',
 }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Website',
+    name: 'Ly Tran Hoang Hieu',
+    url: `${process.env.NEXTAUTH_URL}`,
+    description: 'Build NextJs13 app with Material UI to clone SoundCLoud',
+  }
   return (<>
     <AppHeader />
     {children}
     <AppBottom />
+    <script
+      data-nscript="lazyOnload"
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
   </>
   );
 }
